b1b98fe9c36823506370bdf6e57d735f6408648e
mexsov <151562135+mexsov@users.noreply.github.com>
2024-11-12 08:21:51 +0200

    first push

---
+++ Dukstyna-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+function App() {
+  
+
+  return (
+    <>
+      
+      {/* <h1>Vite + React</h1> */}
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ Dukstyna-frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+
+const Header = () => {
+    
+    <div className="header">
+
+        <div className="header picture">
+            <img src="" alt="mokyklos-foto" />
+        </div>
+        <div className="header tekstas">
+            <ul>
+                <li>Naujienos</li>
+                <li>Veiklos sritys</li>
+                <li>Pagalba mokiniui</li>
+                <li>Finansinės ataskaitos</li>
+                <li>Karjeros ugdymo centras</li>
+                <li>Struktūra ir kontaktai</li>
+            </ul>
+        </div>
+    </div>
+}
+
+
+export default Header
