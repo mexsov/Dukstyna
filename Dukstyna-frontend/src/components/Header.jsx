6f5d5dc774bb5554dc1bcf1349ccde17c2dc8468
mexsov <151562135+mexsov@users.noreply.github.com>
2024-11-12 09:03:15 +0200

    working header

--- Dukstyna-frontend/src/components/Header.jsx
+++ Dukstyna-frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
-
+// import React from "react";
+import {Link} from "react-router-dom"
 
 const Header = () => {
     
+
+    return (
     <div className="header">
 
         <div className="header picture">
@@ -9,15 +12,17 @@ const Header = () => {
         </div>
         <div className="header tekstas">
             <ul>
-                <li>Naujienos</li>
-                <li>Veiklos sritys</li>
-                <li>Pagalba mokiniui</li>
-                <li>Finansinės ataskaitos</li>
-                <li>Karjeros ugdymo centras</li>
-                <li>Struktūra ir kontaktai</li>
+                
+                <li><Link to="/"></Link>Naujienos</li>
+                <li><Link to="/Veiklos sritys"></Link>Veiklos sritys</li>
+                <li><Link to="/Pagalba mokiniui"></Link>Pagalba mokiniui</li>
+                <li><Link to="/Finansinės ataskaitos"></Link>Finansinės ataskaitos</li>
+                <li><Link to="/Karjeros ugdymo centras"></Link>Karjeros ugdymo centras</li>
+                <li><Link to="/Struktūra ir kontaktai"></Link>Struktūra ir kontaktai</li>
             </ul>
         </div>
     </div>
+    );
 }
 
 
